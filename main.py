71ad0e48fedb5fb442ed974dfe82ba60b84d55b9
Taha <tbendjedia@gmail.com>
2026-01-30 22:39:45 +0100

    Eye_Mouse.1

--- main.py
+++ main.py
@@ -1,21 +1,52 @@
 import mediapipe as mp
 import cv2
+import pyautogui
 
 cam = cv2.VideoCapture(0)
-face_mesh = mp.solutions.face_mesh.FaceMesh()
+face_mesh = mp.solutions.face_mesh.FaceMesh(refine_landmarks=True)
+screen_w , screen_h = pyautogui.size()
+blinked = False
 while True:
  _, frame = cam.read()
+ frame = cv2.flip(frame,1)
  frame_h , frame_w,_ = frame.shape
  rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
  output = face_mesh.process(rgb_frame)
  landmark_points = output.multi_face_landmarks
  if landmark_points:
      landmarks = landmark_points[0].landmark
-     for landmark in landmarks:
+     for id , landmark in enumerate(landmarks[474:478]) :
          x= int(landmark.x * frame_w)
          y = int(landmark.y * frame_h)
          cv2.circle(frame,(x,y),3,(0,255,0))
-         print(x,y)
+         if id ==1:
+             screen_x = screen_w * landmark.x
+             screen_y = screen_h * landmark.y
+             pyautogui.moveTo(screen_x,screen_y)
+     left = [landmarks[145] , landmarks[159]]
+     for landmark in left:
+         x= int(landmark.x * frame_w)
+         y = int(landmark.y * frame_h)
+         cv2.circle(frame,(x,y),3,(0,255,255))
+         
+         
+         if (left[0].y - left[1].y) < 0.0095:
+            # ONLY click if we haven't clicked already for this specific blink
+             if blinked == False:
+                pyautogui.click()
+                blinked = True 
+                print("Click!")
+         else:
+            # If the eye is open, reset the flag so we can click again later
+            blinked = False
+            
+         
+         
+     
+    
+             
+    
+         
          
  cv2.imshow('Eye Mouse', frame)
  cv2.waitKey(1)
